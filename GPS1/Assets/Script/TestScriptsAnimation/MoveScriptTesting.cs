b9efbb623a5c71202225f57bde2372edba374a7e
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-06-06 17:01:39 +0800

    6/6UpdateOnAnimationandAudio

---
+++ GPS1/Assets/Script/TestScriptsAnimation/MoveScriptTesting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScriptTesting : MonoBehaviour
+{
+    public float Speeds = 0.0f;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(Speeds));
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Speeds = 1f;
+            FindObjectOfType<AudioManager>().Play("AudioTest");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            Speeds = 0.0f;
+            FindObjectOfType<AudioManager>().Pause("AudioTest");
+        }
+    }
+}
