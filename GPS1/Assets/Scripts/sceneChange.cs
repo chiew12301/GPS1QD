dffa8e93080d65bdea529819a39a42c42a3d4666
Arithoas <65888376+Arithoas@users.noreply.github.com>
2020-06-11 20:54:43 +0800

    Map transitioning

---
+++ GPS1/Assets/Scripts/sceneChange.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime;
+    private doorTouch door;
+    public string levelName;
+
+    private void Start()
+    {
+        GameObject d = GameObject.FindGameObjectWithTag("Trigger");
+        door = d.GetComponent<doorTouch>();
+
+    }
+    public void changeScene()
+    {
+        if (door.playerTouch)
+        {
+            StartCoroutine(LoadLevel());
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelName);
+    }
+
+}
