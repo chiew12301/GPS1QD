71d3d929ec3e23020c6c5f584326e0c45839e0b2
chiew12301 <64896661+chiew12301@users.noreply.github.com>
2020-06-12 01:36:38 +0800

    Night update

---
+++ GPS1/Assets/Script/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> itemLists; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        itemLists = new List<Item>();
+        Debug.Log("ItemSlots Created");
+    }
+
+    public void addItem(Item i)
+    {
+        if(i.name == "book")
+        {
+            i.desc = "Meh just a book";
+        }
+        itemLists.Add(i);
+        Debug.Log("Item Get");
+        Debug.Log(i.name);
+    }
+}
---
+++ GPS1/Assets/Script/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item
+{
+    public Sprite itemSprite;
+    public string name;
+    public string desc;
+
+}
