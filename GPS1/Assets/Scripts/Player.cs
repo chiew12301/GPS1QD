db0454f22463cd11ad8edc76731629f9d84ddf52
Arithoas <65888376+Arithoas@users.noreply.github.com>
2020-06-13 12:20:07 +0800

    Rudimentary save system
    It still has some issues but it works
    
    When loading it doesnt load to the place the player saved at it just loads the scene
    
    It cant load things like inventory yet since I dont have the inventory system stuff

---
+++ GPS1/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int curScene;
+    private bool loaded = false;
+    Vector3 loadPosition;
+    //private savePointTouch bench;
+    private void Start()
+    {
+        curScene = SceneManager.GetActiveScene().buildIndex;
+        //GameObject savePoint = GameObject.FindGameObjectWithTag("Trigger");
+        //bench = savePoint.GetComponent<savePointTouch>();
+
+        if (loaded)
+        {
+            transform.position = loadPosition;
+        }
+    }
+
+    public void savePlayer()
+    {
+        saveSystem.Save(this);
+    }
+
+    public void loadPlayer()
+    {
+        playerData data = saveSystem.Load();
+        loaded = true;
+        loadPosition.x = data.position[0];
+        loadPosition.y = data.position[1];
+        loadPosition.z = data.position[2];
+        Debug.LogError("It works");
+        SceneManager.LoadScene(data.curScene);
+    }
+}
---
+++ GPS1/Assets/Scripts/playerData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class playerData
+{
+    public int curScene;
+    public float[] position;
+    
+
+    public playerData(Player player)
+    {
+        curScene = player.curScene;
+
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
