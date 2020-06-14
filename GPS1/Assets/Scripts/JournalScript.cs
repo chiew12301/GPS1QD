98ba4fa9404879d961913427a7f0db3ea92cccba
Wan Hazwan <wahazwan1@gmail.com>
2020-06-14 18:12:30 +0800

    Journal
    Player's Journal

---
+++ GPS1/Assets/Scripts/JournalScript.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class JournalScript : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public GameObject FirstNote;
+    public GameObject SecondNote;
+    public GameObject ThirdNote;
+
+
+    public Button JournalButton;
+    public Button PrevButton;
+    public Button NextButton;
+
+    public GameObject PrevPage;
+    public GameObject NextPage;
+    private int Page = 1;
+
+    //Journal Tabs
+    public GameObject NoteDetails;
+    public GameObject NoteTab;
+    public GameObject NoteTabBig;
+    public Button NoteTabSmall;
+
+    public GameObject SettingsDetails;
+    public GameObject SettingsTab;
+    public GameObject SettingsTabBig;
+    public Button SettingsTabSmall;
+    
+    
+
+    void Update()
+    {
+        pageVisibility();
+    }
+    public void ExitJournal()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+        ResetNotes();
+        JournalButton.interactable = true;
+    }
+
+    public void OpenJournal()
+    {
+        OpenNoteTab();
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+
+        FirstNote.SetActive(true);
+        SecondNote.SetActive(false);
+
+        //PrevButton.Select();
+        JournalButton.interactable = false;
+
+        Page = 1;
+    }
+
+    public void OpenNoteTab()
+    {
+        NoteTab.SetActive(false);
+        NoteTabBig.SetActive(true);
+        SettingsTabBig.SetActive(false);
+        SettingsTab.SetActive(true);
+
+        NoteDetails.SetActive(true);
+        SettingsDetails.SetActive(false);
+    }
+
+    public void Note_1()
+    {
+        ResetNotes();
+        FirstNote.SetActive(true);
+    }
+
+    public void Note_2()
+    {
+        ResetNotes();
+        SecondNote.SetActive(true);
+    }
+
+    public void Note_3()
+    {
+        ResetNotes();
+        ThirdNote.SetActive(true);
+    }
+
+    public void OpenSettings()
+    {
+        NoteTab.SetActive(true);
+        NoteTabBig.SetActive(false);
+        SettingsTabBig.SetActive(true);
+        SettingsTab.SetActive(false);
+
+        NoteDetails.SetActive(false);
+        SettingsDetails.SetActive(true);
+    }
+
+    public void pageVisibility()
+    {
+        if(Page == 1)
+        {
+            NextPage.SetActive(true);
+            PrevPage.SetActive(false);
+        }
+        else if(Page == 2)
+        {
+            NextPage.SetActive(true);
+            PrevPage.SetActive(true);   
+        }
+        else if(Page == 3)
+        {
+            NextPage.SetActive(false);
+            PrevPage.SetActive(true);
+        }
+    }
+
+    public void GoNextPage()
+    {
+        if (Page == 1)
+        {
+            Note_2();
+            Page = 2;
+        }
+        else if(Page == 2)
+        {
+            Note_3();
+            Page = 3;
+        }
+        else if (Page == 3)
+        {
+            //nothing
+        }
+
+    }
+
+    public void GoPrevPage()
+    {
+        if (Page == 1)
+        {
+            //nothing
+        }
+        else if (Page == 2)
+        {
+            Note_1();
+            Page = 1;
+        }
+        else if (Page == 3)
+        {
+            Note_2();
+            Page = 2;
+        }
+    }
+
+    private void ResetNotes()
+    {
+        FirstNote.SetActive(false);
+        SecondNote.SetActive(false);
+        ThirdNote.SetActive(false);
+    }
+}
---
+++ GPS1/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public static bool GameisPaused = false;
+    public GameObject pauseMenuUI;
+
+    public GameObject SettingsMenuUI;
+
+    public Button PauseButton;
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPaused = true;
+        PauseButton.interactable = false;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPaused = false;
+        PauseButton.interactable = true;
+
+    }
+
+    public void MainMenu()
+    {
+        //SceneManager.LoadScene("Menu");
+        Debug.Log("Returning to Main Menu");
+    }
+
+    public void Volume()
+    {
+        SettingsMenuUI.SetActive(true);
+    }
+
+    public void ExitSettings()
+    {
+        SettingsMenuUI.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quitting game");
+        //Application.Quit();
+    }
+
+}
