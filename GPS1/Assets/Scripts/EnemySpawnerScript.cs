09ae594b6d858a8bf229cb82d5ac6253436548f5
Wan Hazwan <wahazwan1@gmail.com>
2020-06-21 21:29:38 +0800

    EnemySpawn
    Spawn enemy

---
+++ GPS1/Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    public GameObject enemy;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+
+    void Update()
+    {
+        //Spawning enemy - Starts
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randX = Random.Range(-0.8f, 8.0f); //Spawn between the x coordinate
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+        //Spawning enemy - Ends
+    }
+}
